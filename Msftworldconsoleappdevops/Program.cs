f530752867b37f66ff762da6ccf0f608e4177e96
revben23 <1989revben23@users.noreply.github.com>
2018-01-03 03:33:21 -0500

    M

--- Msftworldconsoleappdevops/Program.cs
+++ Msftworldconsoleappdevops/Program.cs
@@ -14,5 +14,14 @@ namespace Msftworldconsoleappdevops
             Console.WriteLine("Hello World, my name is {0}, I am {1}.", name, age);
             Console.ReadKey();
         }
+
+        static void Main2(string[] args)
+        {
+            string appNName = "Numbergusser";
+            string appversion = "1.0.0";
+            string author = "Revi Bennett";
+            Console.WriteLine ("{0}: Version {1} by {2 }.", appNName, appversion, author);
+
+        }
     }
 }
